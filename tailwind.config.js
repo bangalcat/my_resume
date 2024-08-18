7fb3eb84fb947fe07d4e5a5b738ddbd8b70ce76d
Donghyun <semaj9310@gmail.com>
2024-08-19 01:14:09 +0900

    upgrade tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: ["./public/index.html", "./index.html"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
